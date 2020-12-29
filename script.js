177ca4846871d6c7e2ffc7e07da2451f4f04fa4e
himanshu1498 <himanshunimkar14@gmail.com>
2020-12-29 23:46:52 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function slideshow(){
+
+    var x = document.getElementById('check-class');
+    if (x.style.display=== "none"){
+        x.style.display="Block";
+    }else{
+        x.style.display="none";
+    }
+}
